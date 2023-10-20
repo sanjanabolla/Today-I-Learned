ca2318d0b3ec1cc076a191b28b72a68e3b8593fc
sanjanab5 <sanjana.bolla5@gmail.com>
2023-10-20 15:42:53 -0400

    Adding Disputed and CSS final updates

--- src/App.js
+++ src/App.js
@@ -146,7 +146,7 @@ function isValidHttpUrl(string) {
 
 function NewFactForm({ setFacts, setShowForm }) {
   const [text, setText] = useState("");
-  const [source, setSource] = useState("http://example.com");
+  const [source, setSource] = useState("");
   const [category, setCategory] = useState("");
   const [isUploading, setIsUploading] = useState(false);
   const textLength = text.length;
@@ -274,6 +274,8 @@ function FactList({ facts, setFacts }) {
 
 function Fact({ fact, setFacts }) {
   const [isUpdating, setIsUpdating] = useState(false);
+  const isDisputed =
+    fact.votesInteresting + fact.votesMindblowing < fact.votesFalse;
 
   async function handleVote(columnName) {
     setIsUpdating(true);
@@ -293,6 +295,7 @@ function Fact({ fact, setFacts }) {
   return (
     <li className="fact">
       <p>
+        {isDisputed ? <span className="disputed">[⛔️DISPUTED] </span> : null}
         {fact.text}
         <a className="source" href={fact.source} target="_blank">
           (Source)
